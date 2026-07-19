80142b62c2fedc31aca33cee29f7d4fcbef8cdf8
Har0ld0 <gdmartinperez@gmail.com>
2026-07-19 11:20:07 +0200

    S: Fixed site generation

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ from site_generator import cleaning_dir, copy_to_dir, generate_page_recursive
 
 def main():
     basepath = "/"
-    if sys.argv[0] is not None:
-        basepath = sys.argv[0]
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    print(basepath)
     print("Cleaning /docs and copying /static into /docs")
     cleaning_dir("docs/")
     copy_to_dir("static/", "docs/")
